8b7a7f600f8d069dc0c74428a1d65810ef060a67
klion9280 <lone8877@gmail.com>
2023-02-07 08:32:13 +0800

    0207

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 import users from '../models/users.js'
+import jwt from 'jsonwebtoken'
 
 export const register = async (req, res) => {
   try {
@@ -16,5 +17,65 @@ export const register = async (req, res) => {
     } else {
       res.status(500).json({ success: false, message: '未知錯誤' })
     }
+  }
+}
+
+export const login = async (req, res) => {
+  try {
+    const token = jwt.sign({ _id: req.user._id }, process.env.JWT_SECRET, { expiresIn: '7 days' })
+    req.user.tokens.push(token)
+    await req.user.save()
+    res.status(200).json({
+      success: true,
+      message: '',
+      result: {
+        token,
+        account: req.user.account,
+        email: req.user.email,
+        cart: req.user.cart.length,
+        role: req.user.role
+      }
+    })
+  } catch (error) {
+    res.status(500).json({ success: false, message: '未知錯誤' })
+  }
+}
+
+export const logout = async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(token => token !== req.token)
+    await req.user.save()
+    res.status(200).json({ success: true, message: '' })
+  } catch (error) {
+    res.status(500).json({ success: false, message: '未知錯誤' })
+  }
+}
+
+export const extend = async (req, res) => {
+  try {
+    const idx = req.user.tokens.findIndex(token => token === req.token)
+    const token = jwt.sign({ _id: req.user._id }, process.env.JWT_SECRET, { expiresIn: '7 days' })
+    req.user.tokens[idx] = token
+    await req.user.save()
+    res.status(200).json({ success: true, message: '', result: token })
+  } catch (error) {
+    res.status(500).json({ success: false, message: '未知錯誤' })
+  }
+}
+
+export const getUser = (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      message: '',
+      result: {
+        account: req.user.account,
+        email: req.user.email,
+        cart: req.user.cart.length,
+        role: req.user.role
+      }
+    })
+  } catch (error) {
+    res.status(500).json({ success: false, message: '未知錯誤' })
   }
 }
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import userRoute from './routes/users.js'
+import './passport/passport.js'
 
 mongoose.connect(process.env.DB_URL)
 mongoose.set('sanitizeFilter', true)
